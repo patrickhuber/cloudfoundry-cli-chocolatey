6d921cfbd8e38b26a7c2f16bb52daf37930afdf6
Patrick Huber <patrickhuber@hotmail.com>
2017-12-27 14:43:47 -0500

    installs dependency in container and uses envsubst

--- ci/pack.sh
+++ ci/pack.sh
@@ -2,16 +2,24 @@
 export CF_VERSION=$(cat cf-cli-release/version)
 export CF_DESCRIPTION=$(cat cf-cli-release/body)
 
+# install envsubst
+apt-get update
+apt-get -y install gettext-base
+apt-get clean 
+rm -rf /var/lib/apt/lists/*
+
 # create working directory
 mkdir stage/tools -p
 
 # create nuspec file
-cat cloudfoundry-cli-chocolatey/template/cf-cli.nuspec \
-| envsubst > stage/cf-cli.nuspec
+envsubst \
+< cloudfoundry-cli-chocolatey/template/cf-cli.nuspec \
+> stage/cf-cli.nuspec
 
 # create the install script
-cat cloudfoundry-cli-chocolatey/template/chocolateyinstall.ps1 \
-| envsubst > stage/tools/chocolateyinstall.ps1
+envsubst \
+< cloudfoundry-cli-chocolatey/template/chocolateyinstall.ps1 \
+> stage/tools/chocolateyinstall.ps1
 
 # run choco pack
 # choco pack stage
