001da5e4c4267a920fdf05a5a0aecb1f8d0bb29f
Patrick Huber <patrickhuber@hotmail.com>
2017-12-27 16:44:55 -0500

    updates pack shell script to us ampersand for tools filter replacement

--- ci/pack.sh
+++ ci/pack.sh
@@ -8,14 +8,14 @@ mkdir stage/tools -p
 # create nuspec file
 sed \
  -e 's/$CF_VERSION/'"$CF_VERSION"'/' \
- -e 's/$PACKAGE_TOOLS_FILTER/'"$PACKAGE_TOOLS_FILTER"'/' \
+ -e 's@$PACKAGE_TOOLS_FILTER@'"$PACKAGE_TOOLS_FILTER"'@' \
  cloudfoundry-cli-chocolatey/template/cf-cli.nuspec \
 > stage/cf-cli.nuspec
 
 # create the install script
 sed \
  -e 's/$CF_VERSION/'"$CF_VERSION"'/' \
- cloudfoundry-cli-chocolatey/template/chocolateyinstall.ps1 
+ cloudfoundry-cli-chocolatey/template/chocolateyinstall.ps1 \
 > stage/tools/chocolateyinstall.ps1
 
 # run choco pack
